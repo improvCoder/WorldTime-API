cb4a3a7a4c41cc101cf1b60016c08e9f099b9dfc
improvCoder <166443448+improvCoder@users.noreply.github.com>
2024-04-26 18:27:15 -0700

    World Time API Usage
    Extra functionality. Parsed the JSON data to extract time and date separately. Also added the list of available Cities.
    The code for creating the list of Available Cities was borrowed from W3 schools.

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 
 function myfunction(){
 
+	let time="";
+	let date="";
+	var city=document.getElementById('user_Input').value;
+	const url='https://cors-anywhere.herokuapp.com/http://worldtimeapi.org/api/timezone/'+city
 
-var city=document.getElementById('user_Input').value;
-	const url1='https://cors-anywhere.herokuapp.com/http://worldtimeapi.org/api/timezone/America/'+city
-	//const url='https://cors-anywhere.herokuapp.com/http://worldtimeapi.org/api/timezone'
 
-
-fetch(url1,{
+fetch(url,{
 		headers:{
 			'Authorization':'Bearer${API_KEY}'
 		}
@@ -20,13 +20,52 @@ fetch(url1,{
 		return response.json();
 	})
 	.then(data =>{
-		console.log(data);
-		document.getElementById("city").innerHTML=data.timezone;
-		document.getElementById("time").innerHTML=data.datetime;
+		
+		console.log(data)
+
+	document.getElementById("city").innerHTML=data.timezone;
+		let s=data.datetime;
+		
+		for(i=0;i<10;i++)
+			{date=date+s[i]}
+			document.getElementById("date").innerHTML=date;
+	
+		for(i=11;i<19;i++)
+			{time=time+s[i]}
+		document.getElementById("time").innerHTML=time;
 	})
 	.catch(error=> {
 		console.error('Error was found',error);
 	
 	});
+
+}
+
+
+function list_of_Cities(){
+
+	const url1='https://cors-anywhere.herokuapp.com/http://worldtimeapi.org/api/timezone'
+	const array=[];
+
+	fetch(url1)
+	.then(response => response.json())
+	.then(data1 => {
+
+					for(i=0;i<350;i++)
+					{
+					array.push(data1[i]);
+					
+					}
+					console.log(array)
+					const add= document.createDocumentFragment();
+					for(let x in array){
+						const li= document.createElement('li');
+						li.textContent= array[x];
+						add.appendChild(li);
+					}
+					document.getElementById('cities').appendChild(add);
+	})
+
+	.catch(error => console.error(error))	
 
 }
